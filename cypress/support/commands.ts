ffe95c276ccd998f8b14cdbeb33d56a92ef9030e
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-09-21 10:54:10 +0200

    sep03

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,6 +1,19 @@
-Cypress.Commands.add('login', (username: string, password: string) => { 
+Cypress.Commands.add('login', (username: string, password: string) => {
+    // 1. cy.request logowania
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:4001/users/signin',
+        body: {
+            username: username,
+            password: password,
+        },
+    }).then((response) => {
+        // 2. ustawimy odpowiedź w localStorage
+        localStorage.setItem('user', JSON.stringify(response.body))
+        // 3. ustawimy ciastko 'token' z wartością tokena z odpowiedzi
+        cy.wrap(response.body.token).as('jwtToken')
+        cy.setCookie('token', response.body.token)
+    })
+    // 4. wejdziemy na stronę główną i powinniśmy być zalogowani
     cy.visit('/')
-    cy.get('.form-control').eq(0).type(username)
-    cy.get('.form-control').eq(1).type(password)
-    cy.get('.btn-primary').click()
- })
+})
