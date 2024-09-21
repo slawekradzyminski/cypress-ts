5e12406c86dc1cf2ac6ee8d0b1048da41ed125ce
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-09-21 12:54:43 +0200

    sep08

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,16 +1,29 @@
 /// <reference types="cypress" />
 
+import { getRandomUser } from "../generators/userGenerator"
+
 describe('Login page tests', () => {
   beforeEach(() => {
     cy.visit('/')
   })
 
   it('should login successfully', () => {
-    cy.get('.form-control').eq(0).type(Cypress.env('login'))
-    cy.get('.form-control').eq(1).type(Cypress.env('password'))
+    // given - setup testu
+    const user = getRandomUser()
+    cy.log(JSON.stringify(user, null, 2))
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:4001/users/signup',
+      body: user
+    })
+
+    // when - testowana akcja
+    cy.get('.form-control').eq(0).type(user.username)
+    cy.get('.form-control').eq(1).type(user.password)
     cy.get('.btn-primary').click()
 
-    cy.get('h1').should('contain.text', 'Slawomir')
+    // then - asercje
+    cy.get('h1').should('contain.text', user.firstName)
   })
 
   it('should fail to login', () => {
