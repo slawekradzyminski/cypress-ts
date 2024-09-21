5705d1890f04fbc0eedb801b5a23461441ae399b
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-09-21 14:07:29 +0200

    sep11g

--- cypress/e2e/edit.cy.ts
+++ cypress/e2e/edit.cy.ts
@@ -1,11 +1,20 @@
+import { User } from "../../types/registerTypes"
+import { getRandomUser } from "../generators/userGenerator"
+
 describe('Edit page tests', () => {
+  let user: User
+
     beforeEach(() => {
-      cy.login(Cypress.env('login'), Cypress.env('password'))
+      user = getRandomUser()
+      cy.register(user)
+      cy.login(user.username, user.password)
       cy.visit('/')
     })
   
     it('should edit', () => {
-
+      // given
+      
+      // open edit page
     })
   
   })
