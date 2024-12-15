b55193317f172e8d4c5935279d44d6e81a441eed
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-12-15 08:10:09 +0100

    fix bug

--- cypress/generators/userGenerator.ts
+++ cypress/generators/userGenerator.ts
@@ -2,12 +2,25 @@ import { User } from "../types/registerTypes";
 import { faker } from '@faker-js/faker';
 import { Role } from "../types/roles";
 
+const getValidString = (generator: () => string, minLength = 4): string => {
+    const maxAttempts = 20;
+    let attempts = 0;
+    let value = generator();
+    
+    while (value.length < minLength && attempts < maxAttempts) {
+        value = generator();
+        attempts++;
+    }
+    
+    return value.length >= minLength ? value : value + '0'.repeat(minLength - value.length);
+};
+
 export const getRandomUser = (): User => {
     return {
-        username: faker.internet.userName(),
-        password: faker.internet.password(),
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
+        username: getValidString(() => faker.internet.userName()),
+        password: getValidString(() => faker.internet.password()),
+        firstName: getValidString(() => faker.person.firstName()),
+        lastName: getValidString(() => faker.person.lastName()),
         email: faker.internet.email(),
         roles: [Role.ROLE_ADMIN, Role.ROLE_CLIENT]
     }
