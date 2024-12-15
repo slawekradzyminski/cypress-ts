b1fd5b04e1711a35913733800623c90af33c09b8
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-12-15 10:54:38 +0100

    allure report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "cypress";
+import { allureCypress } from "allure-cypress/reporter";
 
 export default defineConfig({
   e2e: {
@@ -13,6 +14,10 @@ export default defineConfig({
         }
         return launchOptions
       })
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
   },
 });
